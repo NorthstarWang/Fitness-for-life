fe8c0fe4438225715375b492fbc892a8c011e7d9
NorthstarWang <61356439+NorthstarWang@users.noreply.github.com>
2020-12-03 17:03:51 +0800

    User avatar half done

--- BLL/User.py
+++ BLL/User.py
@@ -1,6 +1,9 @@
 from app import *
 from models import User
 from flask import request
+import base64
+from io import BytesIO
+from PIL import Image
 
 
 @app.route('/profile')
@@ -14,8 +17,11 @@ def profile():
 def edit_description():
     description = str(request.form['description'])
     user_id = int(request.form['id'])
+    avatar = request.files['avatar']
     user = User.query.filter_by(id=user_id).first()
     user.description = description
+    avatar.save('../static/user/icon/'+avatar.filename)
+    user.icon = avatar.filename
     try:
         db.session.commit()
         return 'success'

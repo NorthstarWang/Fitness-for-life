fb919a6a37c554b1aa392975c7bbd10a5049fbbf
NorthstarWang <61356439+NorthstarWang@users.noreply.github.com>
2020-12-04 13:02:00 +0800

    User avatar edit available

--- BLL/User.py
+++ BLL/User.py
@@ -1,13 +1,24 @@
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms import FileField, Form, SubmitField, StringField, HiddenField
+from wtforms.validators import DataRequired
+
 from app import *
 from models import User
-from flask import request
+from flask import request, redirect, url_for
+
+
+class AvatarForm(Form):
+    avatar = FileField('profile_avatar', validators=[FileRequired(), FileAllowed(['png', 'jpg', 'jpeg'])])
+    id = HiddenField('id')
+    submit = SubmitField('Confirm')
 
 
 @app.route('/profile')
 def profile():
     user_id = request.args.get('id')
     user = User.query.filter_by(id=user_id).first()
-    return render_template('profile.html', user=user)
+    form = AvatarForm()
+    return render_template('profile.html', user=user, form=form)
 
 
 @app.route('/api/profile/edit/description', methods=['POST', 'GET'])
@@ -21,3 +32,15 @@ def edit_description():
         return 'success'
     except Exception:
         return 'failure'
+
+
+@app.route('/api/profile/edit/avatar', methods=['POST', 'GET'])
+def edit_avatar():
+    form = AvatarForm()
+    avatar = request.files.get('avatar')
+    avatar.save(os.path.join(app.root_path, 'static/user/icon/' + avatar.filename))
+    user_id = str(request.form['id'])
+    user = User.query.filter_by(id=user_id).first()
+    user.icon = avatar.filename
+    db.session.commit()
+    return redirect(url_for('profile', id=user_id))

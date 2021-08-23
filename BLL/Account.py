c22c24c1f9d1be617d37d7f289b8653510542e04
Wang Yang <1527638985@qq.com>
2021-08-23 00:55:50 -0400

    Azure adaption

--- BLL/Account.py
+++ BLL/Account.py
@@ -7,13 +7,13 @@ from flask_mail import Mail, Message
 from app import render_template, app
 from models import *
 
-urlSerializer = URLSafeTimedSerializer('Thisisasecret!')
+urlSerializer = URLSafeTimedSerializer(os.getenv('URL_SECRET'))
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USE_TLS"] = False
-app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_USERNAME"] = os.getenv('MAIL_USERNAME')
+app.config["MAIL_PASSWORD"] = os.getenv('MAIL_PASSWORD')
 mailer = Mail(app)
 
 account = Blueprint("account", __name__, url_prefix="/account")
